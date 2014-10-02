6133176a3e19872b5325472f9faabbe74079e009
Deivid Azevedo <deividazevedo1@gmail.com>
2014-10-02 19:11:38 -0300

    Inicialização da tela Sobre...

--- CalcNote/src/br/com/qnota/MainActivity.java
+++ CalcNote/src/br/com/qnota/MainActivity.java
@@ -15,13 +15,15 @@ import android.widget.TextView;
 @SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
-	TextView tvQuantidade, tvMediaGeral, tvTituloConfig;
+	TextView tvQuantidade, tvMediaGeral, tvTituloConfig, tvTituloSobre,
+			tvDescricao, tvComoConfigurar, tvQuantidadeSobre,
+			tvDescricaoConfiguracao, tvObterResult, tvDescricaoResult;
 	EditText etMediaOutra;
 	RadioGroup rgNotas, rgMediaGeralAdotada;
 	int quantidadeDeNotas, mediaGeral;
 	RadioButton rbUma, rbDuas, rbTres, rbQuatro, rbMediaSete, rbMediaOutra;
 	ImageButton btCalcular, btConfigurar, btSobre;
-	Button btConfigVoltar, btConfigSalvar;
+	Button btConfigVoltar, btConfigSalvar, btSobreVoltar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +53,17 @@ public class MainActivity extends Activity {
 	}
 
 	/**
+	 * Método que chama a tela configurar, inicializa os objetos desta tela e as
+	 * capturas de botões.
+	 */
+	private void chamaTelaSobre() {
+		setContentView(R.layout.sobre);
+		inicializaObjetos();
+		listeners();
+
+	}
+
+	/**
 	 * Neste método todos os objetos são inicializados de acordo com a tela a
 	 * qual pertencem. É importante perceber que os botões referentes a tela
 	 * inicial estão dentro de um trycatch. Os objetos das outras telas estão
@@ -68,7 +81,7 @@ public class MainActivity extends Activity {
 
 		}
 		try {
-			// INICIALIZA OBJETOS DA TELA CONFIGURA��O
+			// INICIALIZA OBJETOS DA TELA CONFIGURAÇÃO
 			tvTituloConfig = (TextView) findViewById(R.id.tvTituloConfig);
 			tvQuantidade = (TextView) findViewById(R.id.tvQuantidade);
 			rgNotas = (RadioGroup) findViewById(R.id.rgQuantidadeNotas);
@@ -87,6 +100,19 @@ public class MainActivity extends Activity {
 			System.out.println(mediaGeral);
 			btConfigVoltar = (Button) findViewById(R.id.btConfigVoltar);
 			btConfigSalvar = (Button) findViewById(R.id.btConfigSalvar);
+		} catch (Exception e) {
+
+		}
+		try {
+			// INICIALIZA OBJETOS DA TELA SOBRE
+			tvTituloSobre = (TextView) findViewById(R.id.tvTituloSobre);
+			tvDescricao = (TextView) findViewById(R.id.tvDescricao);
+			tvQuantidadeSobre = (TextView) findViewById(R.id.tvQuantidadeSobre);
+			tvComoConfigurar = (TextView) findViewById(R.id.tvComoConfigurar);
+			tvDescricaoConfiguracao = (TextView) findViewById(R.id.tvDescricaoConfiguracao);
+			tvObterResult = (TextView) findViewById(R.id.tvObterResult);
+			tvDescricaoResult = (TextView) findViewById(R.id.tvDescricaoResult);
+			btSobreVoltar = (Button) findViewById(R.id.btVoltar);
 		} catch (Exception e) {
 
 		}
@@ -181,6 +207,18 @@ public class MainActivity extends Activity {
 
 		}
 		try {
+			btSobre.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					chamaTelaSobre();
+
+				}
+			});
+		} catch (Exception e) {
+
+		}
+		try {
 			btConfigVoltar.setOnClickListener(new View.OnClickListener() {
 
 				@Override
@@ -197,6 +235,17 @@ public class MainActivity extends Activity {
 							"Suas configurações foram Salvas com sucesso!");
 					chamaTelaInicial();
 
+				}
+			});
+		} catch (Exception e) {
+
+		}
+		try {
+			btSobreVoltar.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					chamaTelaInicial();
 				}
 			});
 		} catch (Exception e) {
