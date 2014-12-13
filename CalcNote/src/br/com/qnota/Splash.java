0d9409ea6e16bf50f9523d6f16b2342d84e38ab2
Deivid Azevedo <deividazevedo1@gmail.com>
2014-12-13 16:24:14 -0200

    atualizando...

---
+++ CalcNote/src/br/com/qnota/Splash.java
@@ -0,0 +1,24 @@
+package br.com.qnota;
+
+import br.com.qnota.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity implements Runnable {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    public void run(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
